67752571cefb6bc696f98bd436d9b51778f45571
userjona <107501728+userjona@users.noreply.github.com>
2022-11-05 19:55:10 -0300

    Backup exercises.

--- chapter04/for_loop.py
+++ chapter04/for_loop.py
@@ -1,30 +1,30 @@
-# 4.3
-# [print(i + 1) for i in range(0,20)]
+#4.3
+[print(i + 1) for i in range(0,20)]
 
 
-# 4.4
+#4.4
 test = []
-# for i in range(1, 1000001):
-#     test.append(i)
+for i in range(1, 1000001):
+    test.append(i)
 
-# 4.5
-# print(min(test))
-# print(max(test))
+#4.5
+print(min(test))
+print(max(test))
 
-# 4.6
-# [print(i)for i in list(range(1,21,2))]
+#4.6
+[print(i)for i in list(range(1,21,2))]
 
-# 4.7
-# for i in range(1,31):
-#     test.append(i*3)
+#4.7
+for i in range(1,31):
+    test.append(i*3)
 
-# for x in test:
-#     print(x)
+for x in test:
+    print(x)
 
-# 4.8
-# for x in range(1,11):
-#     print(x**3)
+#4.8
+for x in range(1,11):
+    print(x**3)
 
-# 4.9
+#4.9
 [print(x**3) for x in range(1,11)]
 

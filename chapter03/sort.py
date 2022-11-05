c24c6aa57e2ebad463f4df938f00f4d481e051eb
userjona <107501728+userjona@users.noreply.github.com>
2022-11-05 19:51:47 -0300

    Update sort.py

--- chapter03/sort.py
+++ chapter03/sort.py
@@ -1,38 +1,38 @@
 #3.8
 locations = ['Switzerland','Italy','Finland','France','Canada']
 
-# print(locations)
-# print(sorted(locations))
-# print(locations)
-# locations.reverse()
-# print(locations)
-# locations.reverse()
-# print(locations)
-# locations.sort()
-# print(locations)
-# locations.sort(reverse=True)
-# print(locations)
+print(locations)
+print(sorted(locations))
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
 
-# 3.9
+#3.9
 waifus = ['Emilia','Lain','Filo-chan']
 
-# print(len(waifus))
+print(len(waifus))
 
-# 3.10
+#3.10
 random = ['Iwakura','Fiora','Draven','Love','Again','Coffee','Tea','Light']
-# print(random) # random list without sort 
-# random.sort() # permanent sort
-# print(random)
+print(random)  random list without sort 
+random.sort()  permanent sort
+print(random)
 print()
-# print(sorted(random)) # temporally sort
+print(sorted(random))  temporally sort
 print()
-# print(random)
-# random.reverse() # permanent reverse list without sort
-# print(random) 
+print(random)
+random.reverse()  permanent reverse list without sort
+print(random) 
 print()
-# print(random) 
-# random.sort(reverse=True) # permanent reverse list with sort
-# print(random)
+print(random) 
+random.sort(reverse=True)  permanent reverse list with sort
+print(random)
 print()
-# print(sorted(random, reverse=True)) # reverse sorted list
+print(sorted(random, reverse=True))  reverse sorted list
 
